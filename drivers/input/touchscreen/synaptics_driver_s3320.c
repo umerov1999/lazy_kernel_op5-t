8d1879393cd2da38cf88ade28232205523a94e38
Park Ju Hyung <qkrwngud825@gmail.com>
2019-05-06 21:06:07 +0900

    s3320: retry only when the screen is off
    It's unnecessary to follow the retry routine when the system is
    completely up and running as it's for retrieving gesture interrupts
    properly.
    
    Add likely() hints to reduce pipeline cache misses during normal
    operations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -987,6 +987,10 @@ unsigned char addr, unsigned short length, unsigned char *data)
 		},
 	};
 	buf = addr & 0xFF;
+
+	if (likely(!ts_g->is_suspended))
+		return i2c_transfer(client->adapter, msg, 2);
+
 	for (retry = 0; retry < 2; retry++) {
 		if (i2c_transfer(client->adapter, msg, 2) == 2) {
 			retval = length;
@@ -1023,6 +1027,9 @@ unsigned char addr, unsigned short length, unsigned char const *data)
 
 	buf[0] = addr & 0xff;
 	memcpy(&buf[1], &data[0], length);
+
+	if (likely(!ts_g->is_suspended))
+		return i2c_transfer(client->adapter, msg, 1);
 
 	for (retry = 0; retry < 2; retry++) {
 		if (i2c_transfer(client->adapter, msg, 1) == 1) {
