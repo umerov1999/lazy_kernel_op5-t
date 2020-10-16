e6adcad5e0395c0e4e25626f6468792a761b67b2
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-10-16 11:56:08 +0300

    sched/tune: Enable boost_bias for RT cgroup
    Since the RT cgroup is composed of tasks that are latency-sensitive (e.g. Surface flinger, audio playback), let's make sure that they finish soon by putting a boost bias on them so that they are more likely to be placed on the big cluster.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -915,7 +915,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		{ "audio-app",	0, 0, 0 },
 		{ "background",	0, 0, 0 },
 		{ "foreground",	0, 1, 0 },
-		{ "rt",		0, 0, 0 },
+		{ "rt",		0, 1, 0 },
 		{ "top-app",	0, 1, 0 },
 	};
 	int i;
